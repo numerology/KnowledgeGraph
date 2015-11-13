a954c6c9a02890ac0669e087a411a889b7c7d6a2
numerology <jxzheng03@gmail.com>
2015-11-13 15:57:01 -0600

    clear up

--- handlers/API.py
+++ handlers/API.py
@@ -1,1 +1,14 @@
 __author__ = 'Jiaxiao Zheng'
+
+from google.appengine.ext import ndb
+from google.appengine.api import users, files, images
+from google.appengine.ext import blobstore
+from google.appengine.ext.webapp import blobstore_handlers
+
+import webapp2
+import jinja2
+
+class ReturnJSON(webapp2.RequestHandler):
+    def get(self, user_id):
+        
+        return
---
+++ handlers/models.py
@@ -0,0 +1,29 @@
+__author__ = 'Jiaxiao Zheng'
+from google.appengine.ext import ndb
+from google.appengine.api import users, files, images
+from google.appengine.ext import blobstore
+from google.appengine.ext.webapp import blobstore_handlers
+
+import webapp2
+import jinja2
+
+class Reference(ndb.Model):
+    type = ndb.StringProperty()
+    blobkey = ndb.BlobKeyProperty()
+    adddate = ndb.DateTimeProperty(auto_add = True)
+    publishdate = ndb.DateTimeProperty()
+    description = ndb.StringProperty()
+
+
+class Node(ndb.Model):
+    name = ndb.StringProperty()
+    definition = ndb.StringProperty()
+    tags = ndb.StringProperty(repeated = True)
+    childrenIDs = ndb.StringProperty(repeated = True)
+    trending = ndb.IntegerProperty(repeated = True)
+    reference = ndb.StructuredProperty(Reference, repeated = True)
+
+class User(ndb.Model):
+    email = ndb.StringProperty()
+    root = ndb.StructuredProperty()
+
